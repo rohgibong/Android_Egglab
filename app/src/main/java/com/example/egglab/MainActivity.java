f32ba3755d72ea23ae9e6d424fde0c468d4af114
rohgibong <rohgibong@gmail.com>
2024-05-09 11:30:42 +0900

    3일차 commit

--- app/src/main/java/com/example/egglab/MainActivity.java
+++ app/src/main/java/com/example/egglab/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -23,11 +26,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Menu menu = navigationView.getMenu();
+        MenuItem menuItem = menu.findItem(R.id.copyright);
+        SpannableString s = new SpannableString(menuItem.getTitle());
+        s.setSpan(new RelativeSizeSpan(0.6f), 0, s.length(), 0); // 폰트 크기를 상대적으로 조정
+        menuItem.setTitle(s);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
@@ -60,9 +70,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else if(item.getItemId() == R.id.menu6){
 //            수정필요, AboutFragment로 연결되어있음.
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
-        } else if(item.getItemId() == R.id.nav_logout){
-            Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+        } else if(item.getItemId() == R.id.nav_login){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LoginFragment()).commit();
+        } else if(item.getItemId() == R.id.nav_register){
+            Toast.makeText(this, "Register!", Toast.LENGTH_SHORT).show();
         }
+//        else if(item.getItemId() == R.id.nav_logout){
+//            Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+//        }
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
